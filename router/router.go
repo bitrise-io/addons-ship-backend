1299178a8cce77eb1cdb7d76aa3e4723ffe389ef
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-06 09:57:19 +0200

    Add router

---
+++ router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/bitrise-io/addons-ship-backend/services"
+	"github.com/bitrise-io/api-utils/httpresponse"
+	"github.com/bitrise-io/api-utils/middleware"
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+)
+
+// New ...
+func New() *mux.Router {
+	// StrictSlash: allow "trim slash"; /x/ REDIRECTS to /x
+	r := mux.NewRouter(mux.WithServiceName("addons-ship-mux")).StrictSlash(true)
+
+	r.Handle("", middleware.CommonMiddleware().Then(
+		httpresponse.InternalErrHandlerFuncAdapter(services.RootHandler))).Methods("GET", "OPTIONS")
+	return r
+}
