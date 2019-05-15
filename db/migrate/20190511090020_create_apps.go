3bb9fac045390047eddf16ef575b8399b074b541
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-15 15:59:51 +0200

    Make migrations better

--- db/migrate/20190511090020_create_apps.go
+++ db/migrate/20190511090020_create_apps.go
@@ -3,8 +3,6 @@ package migration
 import (
 	"database/sql"
 
-	"github.com/bitrise-io/addons-ship-backend/dataservices"
-	"github.com/bitrise-io/addons-ship-backend/models"
 	"github.com/pressly/goose"
 )
 
@@ -13,19 +11,22 @@ func init() {
 }
 
 func up20190511090020(tx *sql.Tx) error {
-	var err error
-	db := dataservices.GetDB()
-	if !db.HasTable(&models.App{}) {
-		err = db.CreateTable(&models.App{}).Error
-	}
+	_, err := tx.Exec(`CREATE TABLE apps (
+		id uuid primary key NOT NULL,
+		app_slug text NOT NULL,
+		plan text,
+		bitrise_api_token text,
+		api_token text,
+		created_at timestamp with time zone NOT NULL,
+		updated_at timestamp with time zone NOT NULL,
+		deleted_at timestamp with time zone
+	);
+
+	CREATE UNIQUE INDEX apps_app_slug_idx ON apps(app_slug);`)
 	return err
 }
 
 func down20190511090020(tx *sql.Tx) error {
-	var err error
-	db := dataservices.GetDB()
-	if db.HasTable(&models.App{}) {
-		err = db.DropTable(&models.App{}).Error
-	}
+	_, err := tx.Exec(`DROP TABLE apps;`)
 	return err
 }
--- db/migrate/20190513135553_create_app_versions.go
+++ db/migrate/20190513135553_create_app_versions.go
@@ -3,8 +3,6 @@ package migration
 import (
 	"database/sql"
 
-	"github.com/bitrise-io/addons-ship-backend/dataservices"
-	"github.com/bitrise-io/addons-ship-backend/models"
 	"github.com/pressly/goose"
 )
 
@@ -13,19 +11,23 @@ func init() {
 }
 
 func up20190513135553(tx *sql.Tx) error {
-	var err error
-	db := dataservices.GetDB()
-	if !db.HasTable(&models.AppVersion{}) {
-		err = db.CreateTable(&models.AppVersion{}).Error
-	}
+	_, err := tx.Exec(`CREATE TABLE app_versions (
+		id uuid primary key NOT NULL,
+		version text NOT NULL,
+		platform text,
+		build_number text,
+		description text,
+		last_update timestamp with time zone,
+		created_at timestamp with time zone NOT NULL,
+		updated_at timestamp with time zone NOT NULL,
+		deleted_at timestamp with time zone
+	);
+
+	CREATE INDEX app_versions_platform_idx ON app_versions(platform);`)
 	return err
 }
 
 func down20190513135553(tx *sql.Tx) error {
-	var err error
-	db := dataservices.GetDB()
-	if db.HasTable(&models.AppVersion{}) {
-		err = db.DropTable(&models.AppVersion{}).Error
-	}
+	_, err := tx.Exec(`DROP TABLE app_versions;`)
 	return err
 }
