3f3dc9262b015452b1870cd228c66765bea36ed0
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-20 16:12:46 +0200

    Fix migrations

--- db/migrate/20190513135553_create_app_versions.go
+++ db/migrate/20190513135553_create_app_versions.go
@@ -12,18 +12,19 @@ func init() {
 
 func up20190513135553(tx *sql.Tx) error {
 	_, err := tx.Exec(`CREATE TABLE app_versions (
-		id uuid primary key NOT NULL,
-		version text NOT NULL,
-		platform text,
-		build_number text,
-		description text,
-		last_update timestamp with time zone,
-		created_at timestamp with time zone NOT NULL,
-		updated_at timestamp with time zone NOT NULL,
-		deleted_at timestamp with time zone
-	);
+        id uuid primary key NOT NULL,
+        app_id uuid NOT NULL REFERENCES apps (id),
+        version text NOT NULL,
+        platform text,
+        build_number text,
+        description text,
+        last_update timestamp with time zone,
+        created_at timestamp with time zone NOT NULL,
+        updated_at timestamp with time zone NOT NULL,
+        deleted_at timestamp with time zone
+    );
 
-	CREATE INDEX app_versions_platform_idx ON app_versions(platform);`)
+    CREATE INDEX app_versions_platform_idx ON app_versions(platform);`)
 	return err
 }
 
