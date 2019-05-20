2a4e1a46ae0c576d8a950d69926776e65fde6d19
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-20 12:57:53 +0200

    Change migration

--- db/migrate/20190516071347_add_fields_to_app_versions.go
+++ db/migrate/20190516071347_add_fields_to_app_versions.go
@@ -11,11 +11,13 @@ func init() {
 }
 
 func up20190516071347(tx *sql.Tx) error {
-	_, err := tx.Exec(`ALTER TABLE app_versions ADD COLUMN build_slug text;`)
+	_, err := tx.Exec(`ALTER TABLE app_versions ADD COLUMN build_slug text;` +
+		` ALTER TABLE app_versions ADD COLUMN whats_new text;`)
 	return err
 }
 
 func down20190516071347(tx *sql.Tx) error {
-	_, err := tx.Exec(`ALTER TABLE app_versions DROP COLUMN build_slug;`)
+	_, err := tx.Exec(`ALTER TABLE app_versions DROP COLUMN build_slug;` +
+		` ALTER TABLE app_versions DROP COLUMN whats_new;`)
 	return err
 }
