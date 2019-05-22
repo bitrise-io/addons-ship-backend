f369cf16627aea4d79b421b00ad94f8ac669096d
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-22 14:07:37 +0200

    Add AWS provider to env

--- env/env.go
+++ env/env.go
@@ -9,6 +9,7 @@ import (
 	"github.com/bitrise-io/api-utils/logging"
 	"github.com/bitrise-io/api-utils/providers"
 	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
@@ -21,18 +22,21 @@ const (
 
 // AppEnv ...
 type AppEnv struct {
+	Port              string
+	Environment       string
 	Logger            *zap.Logger
 	AppService        dataservices.AppService
 	AppVersionService dataservices.AppVersionService
-	Port              string
-	Environment       string
-	RequestParams     providers.RequestParamsInterface
+	ScreenshotService dataservices.ScreenshotService
 	BitriseAPI        bitrise.APIInterface
+	RequestParams     providers.RequestParamsInterface
+	AWS               providers.AWSInterface
 }
 
 // New ...
-func New(db *gorm.DB) (env AppEnv) {
+func New(db *gorm.DB) (*AppEnv, error) {
 	var ok bool
+	env := &AppEnv{}
 	env.Port, ok = os.LookupEnv("PORT")
 	if !ok {
 		env.Port = "80"
@@ -44,7 +48,40 @@ func New(db *gorm.DB) (env AppEnv) {
 	env.Logger = logging.WithContext(nil)
 	env.AppService = &models.AppService{DB: db}
 	env.AppVersionService = &models.AppVersionService{DB: db}
-	env.RequestParams = &providers.RequestParams{}
+	env.ScreenshotService = &models.ScreenshotService{DB: db}
 	env.BitriseAPI = bitrise.New()
-	return
+	env.RequestParams = &providers.RequestParams{}
+
+	awsConfig, err := awsConfig()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	env.AWS = &providers.AWS{Config: awsConfig}
+	return env, nil
+}
+
+func awsConfig() (providers.AWSConfig, error) {
+	awsBucket, ok := os.LookupEnv("AWS_BUCKET")
+	if !ok {
+		return providers.AWSConfig{}, errors.New("No AWS_BUCKET env var defined")
+	}
+	awsRegion, ok := os.LookupEnv("AWS_REGION")
+	if !ok {
+		return providers.AWSConfig{}, errors.New("No AWS_REGION env var defined")
+	}
+	awsAccessKeyID, ok := os.LookupEnv("AWS_ACCESS_KEY_ID")
+	if !ok {
+		return providers.AWSConfig{}, errors.New("No AWS_ACCESS_KEY_ID env var defined")
+	}
+	awsSecretAccessKey, ok := os.LookupEnv("AWS_SECRET_ACCESS_KEY")
+	if !ok {
+		return providers.AWSConfig{}, errors.New("No AWS_SECRET_ACCESS_KEY env var defined")
+	}
+	return providers.AWSConfig{
+		Bucket:          awsBucket,
+		Region:          awsRegion,
+		AccessKeyID:     awsAccessKeyID,
+		SecretAccessKey: awsSecretAccessKey,
+	}, nil
 }
