e5b4d99aa8bb569dfedbb9bbb2ef5d992db0aa7b
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-24 14:12:35 +0200

    Make create method batch create

--- dataservices/screenshot_service.go
+++ dataservices/screenshot_service.go
@@ -4,6 +4,6 @@ import "github.com/bitrise-io/addons-ship-backend/models"
 
 // ScreenshotService ...
 type ScreenshotService interface {
-	Create(screenshot *models.Screenshot) (*models.Screenshot, []error, error)
+	BatchCreate(screenshot []*models.Screenshot) ([]*models.Screenshot, []error, error)
 	FindAll(appVersion *models.AppVersion) ([]models.Screenshot, error)
 }
--- services/screenshot_service_for_test.go
+++ services/screenshot_service_for_test.go
@@ -3,15 +3,15 @@ package services_test
 import "github.com/bitrise-io/addons-ship-backend/models"
 
 type testScreenshotService struct {
-	createFn  func(*models.Screenshot) (*models.Screenshot, []error, error)
-	findAllFn func(*models.AppVersion) ([]models.Screenshot, error)
+	batchCreateFn func([]*models.Screenshot) ([]*models.Screenshot, []error, error)
+	findAllFn     func(*models.AppVersion) ([]models.Screenshot, error)
 }
 
-func (s *testScreenshotService) Create(screenshot *models.Screenshot) (*models.Screenshot, []error, error) {
-	if s.createFn != nil {
-		return s.createFn(screenshot)
+func (s *testScreenshotService) BatchCreate(screenshot []*models.Screenshot) ([]*models.Screenshot, []error, error) {
+	if s.batchCreateFn != nil {
+		return s.batchCreateFn(screenshot)
 	}
-	panic("You have to override Create function in tests")
+	panic("You have to override BatchCreate function in tests")
 }
 func (s *testScreenshotService) FindAll(appVersion *models.AppVersion) ([]models.Screenshot, error) {
 	if s.findAllFn != nil {
