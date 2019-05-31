d8659eb79822cb03f3750fdd38bc7325579a2a01
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-31 14:27:23 +0200

    Add app store info struct

--- models/app_version.go
+++ models/app_version.go
@@ -1,28 +1,36 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
 
+// AppStoreInfo ...
+type AppStoreInfo struct {
+	ShortDescription string `json:"short_description"`
+	FullDescription  string `json:"full_description"`
+	WhatsNew         string `json:"whats_new"`
+	PromotionalText  string `json:"promotional_text"`
+	Keywords         string `json:"keywords"`
+	ReviewNotes      string `json:"review_notes"`
+	SupportURL       string `json:"support_url"`
+	MarketingURL     string `json:"marketing_url"`
+	NewField         string `json:"new_field"`
+}
+
 // AppVersion ...
 type AppVersion struct {
 	Record
-	Version         string    `json:"version"`
-	Platform        string    `json:"platform"`
-	BuildNumber     string    `json:"build_number"`
-	BuildSlug       string    `json:"build_slug"`
-	LastUpdate      time.Time `json:"last_update"`
-	Description     string    `json:"description"`
-	WhatsNew        string    `json:"whats_new"`
-	PromotionalText string    `json:"promotional_text"`
-	Keywords        string    `json:"keywords"`
-	ReviewNotes     string    `json:"review_notes"`
-	SupportURL      string    `json:"support_url"`
-	MarketingURL    string    `json:"marketing_url"`
-	Scheme          string    `json:"scheme"`
-	Configuration   string    `json:"configuration"`
+	Version          string          `json:"version"`
+	Platform         string          `json:"platform"`
+	BuildNumber      string          `json:"build_number"`
+	BuildSlug        string          `json:"build_slug"`
+	LastUpdate       time.Time       `json:"last_update"`
+	Scheme           string          `json:"scheme"`
+	Configuration    string          `json:"configuration"`
+	AppStoreInfoData json.RawMessage `json:"-" gorm:"column:app_store_info;type:json"`
 
 	AppID uuid.UUID `db:"app_id" json:"-"`
 	App   App       `gorm:"foreignkey:AppID" json:"-"`
@@ -31,5 +39,18 @@ type AppVersion struct {
 // BeforeCreate ...
 func (a *AppVersion) BeforeCreate() error {
 	a.ID = uuid.NewV4()
+	if a.AppStoreInfoData == nil {
+		a.AppStoreInfoData = json.RawMessage(`{}`)
+	}
 	return nil
+}
+
+// AppStoreInfo ...
+func (a *AppVersion) AppStoreInfo() (AppStoreInfo, error) {
+	var appStoreInfo AppStoreInfo
+	err := json.Unmarshal(a.AppStoreInfoData, &appStoreInfo)
+	if err != nil {
+		return AppStoreInfo{}, err
+	}
+	return appStoreInfo, nil
 }
