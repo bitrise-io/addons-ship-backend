6e98f4b26b73eb30bef70955b07ec6e31ec9262d
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-05-31 14:50:12 +0200

    Add more tests for attribute accessor method

---
+++ models/app_version_test.go
@@ -0,0 +1,25 @@
+package models_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bitrise-io/addons-ship-backend/models"
+	"github.com/c2fo/testify/require"
+)
+
+func Test_AppStoreInfo(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		testAppVersion := &models.AppVersion{AppStoreInfoData: json.RawMessage(`{"short_description":"Some shorter description"}`)}
+		appStoreInfo, err := testAppVersion.AppStoreInfo()
+		require.NoError(t, err)
+		require.Equal(t, models.AppStoreInfo{ShortDescription: "Some shorter description"}, appStoreInfo)
+	})
+
+	t.Run("error unmarshaling store info", func(t *testing.T) {
+		testAppVersion := &models.AppVersion{}
+		appStoreInfo, err := testAppVersion.AppStoreInfo()
+		require.EqualError(t, err, "unexpected end of JSON input")
+		require.Equal(t, models.AppStoreInfo{}, appStoreInfo)
+	})
+}
