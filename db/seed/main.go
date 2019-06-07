b2302eb4e94f48b7e7d2502503a924a013e29ee0
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-06-07 10:17:55 +0200

    Add DB seeding from config file

--- db/seed/main.go
+++ db/seed/main.go
@@ -1,11 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
 
 	"github.com/bitrise-io/addons-ship-backend/dataservices"
 	"github.com/bitrise-io/addons-ship-backend/models"
+	"github.com/go-yaml/yaml"
+	"github.com/satori/go.uuid"
 )
+
+type app struct {
+	ID              uuid.UUID `yaml:"id"`
+	Slug            string    `yaml:"slug"`
+	Plan            string    `yaml:"plan"`
+	BitriseAPIToken string    `yaml:"bitrise_api_token"`
+	APIToken        string    `yaml:"api_token"`
+}
+
+type appStoreInfo struct {
+	ShortDescription string `yaml:"short_description"`
+	FullDescription  string `yaml:"full_description"`
+	WhatsNew         string `yaml:"whats_new"`
+	PromotionalText  string `yaml:"promotional_text"`
+	Keywords         string `yaml:"keywords"`
+	ReviewNotes      string `yaml:"review_notes"`
+	SupportURL       string `yaml:"support_url"`
+	MarketingURL     string `yaml:"marketing_url"`
+}
+
+type appVersion struct {
+	ID            uuid.UUID    `yaml:"id"`
+	AppID         uuid.UUID    `yaml:"app_id"`
+	Version       string       `yaml:"version"`
+	Platform      string       `yaml:"platform"`
+	BuildNumber   string       `yaml:"build_number"`
+	BuildSlug     string       `yaml:"build_slug"`
+	LastUpdate    time.Time    `yaml:"last_update"`
+	Scheme        string       `yaml:"scheme"`
+	Configuration string       `yaml:"configuration"`
+	AppStoreInfo  appStoreInfo `yaml:"app_store_info"`
+}
+
+type testData struct {
+	Apps        []app        `yaml:"apps"`
+	AppVersions []appVersion `yaml:"app_versions"`
+}
 
 func main() {
 	err := dataservices.InitializeConnection(dataservices.ConnectionParams{}, true)
@@ -16,41 +59,50 @@ func main() {
 
 	db := dataservices.GetDB()
 
-	// create test apps
-	testApps := []models.App{
-		models.App{
-			AppSlug: "test-app-slug",
-		},
-		models.App{
-			AppSlug: "9447ce3906c65e2d",
-		},
+	var testData testData
+
+	yamlFile, err := ioutil.ReadFile("test_data.yml")
+	if err != nil {
+		log.Printf("Failed to read test_data.yml: %s", err)
 	}
-	for idx := range testApps {
-		if err := db.Create(&testApps[idx]).Error; err != nil {
-			fmt.Printf("Failed to seed db with app: %#v, app: %#v", err, testApps[idx])
+	err = yaml.Unmarshal(yamlFile, &testData)
+	if err != nil {
+		log.Fatalf("Failed to parse test_data.yml: %v", err)
+	}
+
+	// create test apps
+	for _, appData := range testData.Apps {
+		app := models.App{
+			Record:          models.Record{ID: appData.ID},
+			AppSlug:         appData.Slug,
+			APIToken:        appData.APIToken,
+			BitriseAPIToken: appData.BitriseAPIToken,
+			Plan:            appData.Plan,
+		}
+		if err := db.Create(&app).Error; err != nil {
+			fmt.Printf("Failed to seed db with app: %#v, app: %#v", err, app)
 		}
 	}
 
-	// create test app versions for test app
-	for _, testAppVersion := range []models.AppVersion{
-		models.AppVersion{
-			AppID:     testApps[0].ID,
-			Platform:  "ios",
-			BuildSlug: "build-slug-ios",
-		},
-		models.AppVersion{
-			AppID:     testApps[0].ID,
-			Platform:  "android",
-			BuildSlug: "build-slug-android",
-		},
-		models.AppVersion{
-			AppID:     testApps[1].ID,
-			Platform:  "ios",
-			BuildSlug: "42e8d9b1def4074d",
-		},
-	} {
-		if err := db.Create(&testAppVersion).Error; err != nil {
-			fmt.Printf("Failed to seed db with app version: %#v, app version: %#v", err, testAppVersion)
+	// create test app versions
+	for _, appVersionData := range testData.AppVersions {
+		appStoreInfoBytes, err := json.Marshal(appVersionData.AppStoreInfo)
+		if err != nil {
+			fmt.Printf("Failed to marshal app store info: %#v, app store info: %#v", err, appVersionData.AppStoreInfo)
+		}
+		appVersion := models.AppVersion{
+			Record:           models.Record{ID: appVersionData.ID},
+			AppID:            appVersionData.AppID,
+			Platform:         appVersionData.Platform,
+			Version:          appVersionData.Version,
+			BuildNumber:      appVersionData.BuildNumber,
+			LastUpdate:       appVersionData.LastUpdate,
+			Scheme:           appVersionData.Scheme,
+			Configuration:    appVersionData.Configuration,
+			AppStoreInfoData: appStoreInfoBytes,
+		}
+		if err := db.Create(&appVersion).Error; err != nil {
+			fmt.Printf("Failed to seed db with app version: %#v, app version: %#v", err, appVersion)
 		}
 	}
 }
