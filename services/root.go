6ce0201830e2035d328019e13c48f084094123f4
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-01 11:40:21 +0200

    add test things to root handler more print

--- services/root.go
+++ services/root.go
@@ -16,9 +16,9 @@ func RootHandler(env *env.AppEnv, w http.ResponseWriter, r *http.Request) error 
 	eventID := uuid.NewV4()
 	logChunk1 := models.LogChunk{Content: "Something\n"}
 	logChunk2 := models.LogChunk{Content: "Another thing\n"}
-	env.LogStoreService.Set(eventID.String()+"1", logChunk1)
+	fmt.Println(env.LogStoreService.Set(eventID.String()+"1", logChunk1))
 	fmt.Println(env.LogStoreService.Get(eventID.String() + "1"))
-	env.LogStoreService.Set(eventID.String()+"2", logChunk2)
+	fmt.Println(env.LogStoreService.Set(eventID.String()+"2", logChunk2))
 	fmt.Println(env.LogStoreService.Get(eventID.String() + "2"))
 	worker.EnqueueStoreLogToAWS(eventID, 2, "test_path/somethings.log")
 	return httpresponse.RespondWithSuccess(w, map[string]string{"message": "Welcome to Bitrise Ship Addon!"})
