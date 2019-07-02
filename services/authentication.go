14e82fff97751f517aed324f9d984f6c4e62bc61
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-02 15:04:15 +0200

    Add authentication function

--- services/authentication.go
+++ services/authentication.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/bitrise-io/addons-ship-backend/env"
 	"github.com/bitrise-io/api-utils/httpresponse"
@@ -23,6 +24,34 @@ func AuthenticateWithAddonAccessTokenHandlerFunc(env *env.AppEnv, h http.Handler
 		}
 
 		if authToken != env.AddonAccessToken {
+			httpresponse.RespondWithUnauthorizedNoErr(w)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
+// AuthenticateWithDENSecretnHandlerFunc ...
+func AuthenticateWithDENSecretnHandlerFunc(env *env.AppEnv, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		denAuthHeaderKey, ok := os.LookupEnv("BITRISE_DEN_SERVER_ADMIN_SECRET_HEADER_KEY")
+		if !ok || denAuthHeaderKey == "" {
+			httpresponse.RespondWithInternalServerError(w, errors.New("No value set for BITRISE_DEN_SERVER_ADMIN_SECRET_HEADER_KEY env"))
+			return
+		}
+		authToken := r.Header.Get(denAuthHeaderKey)
+		if authToken == "" {
+			httpresponse.RespondWithUnauthorizedNoErr(w)
+			return
+		}
+		denAdminSecret, ok := os.LookupEnv("BITRISE_DEN_SERVER_ADMIN_SECRET")
+		if !ok || denAdminSecret == "" {
+			httpresponse.RespondWithInternalServerError(w, errors.New("No value set for BITRISE_DEN_SERVER_ADMIN_SECRET env"))
+			return
+		}
+
+		if authToken != denAdminSecret {
 			httpresponse.RespondWithUnauthorizedNoErr(w)
 			return
 		}
--- services/authentication_test.go
+++ services/authentication_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/bitrise-io/addons-ship-backend/env"
 	"github.com/bitrise-io/addons-ship-backend/services"
+	"github.com/bitrise-io/go-utils/envutil"
+	"github.com/c2fo/testify/require"
 )
 
 func Test_AuthenticateWithAddonAccessTokenHandlerFunc(t *testing.T) {
@@ -66,4 +68,73 @@ func Test_AuthenticateWithAddonAccessTokenHandlerFunc(t *testing.T) {
 			expectedBody:       `{"message":"Unauthorized"}` + "\n",
 		})
 	})
+}
+
+func Test_AuthenticateWithDENSecretnHandlerFunc(t *testing.T) {
+	for _, tc := range []struct {
+		testName           string
+		denSecretKey       string
+		denSecretValue     string
+		requestHeaders     map[string]string
+		expectedStatusCode int
+		expectedBody       string
+	}{
+		{
+			testName:           "ok",
+			denSecretKey:       "Test-Secret-Key",
+			denSecretValue:     "test-auth-key",
+			requestHeaders:     map[string]string{"Test-Secret-Key": "test-auth-key"},
+			expectedStatusCode: http.StatusOK,
+			expectedBody:       `{"message":"ok"}` + "\n",
+		},
+		{
+			testName:           "when no den secret key is set in envs",
+			denSecretKey:       "",
+			denSecretValue:     "test-auth-key",
+			requestHeaders:     map[string]string{"Test-Secret-Key": "test-auth-key"},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedBody:       `{"message":"Internal Server Error"}` + "\n",
+		},
+		{
+			testName:           "when value in request header is empty",
+			denSecretKey:       "Test-Secret-Key",
+			denSecretValue:     "test-auth-key",
+			requestHeaders:     map[string]string{"Test-Secret-Key": ""},
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedBody:       `{"message":"Unauthorized"}` + "\n",
+		},
+		{
+			testName:           "when no den secret key is set in envs",
+			denSecretKey:       "Test-Secret-Key",
+			denSecretValue:     "",
+			requestHeaders:     map[string]string{"Test-Secret-Key": "test-auth-key"},
+			expectedStatusCode: http.StatusInternalServerError,
+			expectedBody:       `{"message":"Internal Server Error"}` + "\n",
+		},
+		{
+			testName:           "ok",
+			denSecretKey:       "Test-Secret-Key",
+			denSecretValue:     "test-auth-key",
+			requestHeaders:     map[string]string{"Test-Secret-Key": "some-totally-different-key"},
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedBody:       `{"message":"Unauthorized"}` + "\n",
+		},
+	} {
+		t.Run(tc.testName, func(t *testing.T) {
+			revokeSecretKeyFn, err := envutil.RevokableSetenv("BITRISE_DEN_SERVER_ADMIN_SECRET_HEADER_KEY", tc.denSecretKey)
+			require.NoError(t, err)
+			revokeSecretValueFn, err := envutil.RevokableSetenv("BITRISE_DEN_SERVER_ADMIN_SECRET", tc.denSecretValue)
+			require.NoError(t, err)
+
+			performAuthenticationTest(t, "GET", "...", AuthenticationTestCase{
+				requestHeaders:     tc.requestHeaders,
+				authHandlerFunc:    services.AuthenticateWithDENSecretnHandlerFunc,
+				expectedStatusCode: tc.expectedStatusCode,
+				expectedBody:       tc.expectedBody,
+			})
+
+			require.NoError(t, revokeSecretKeyFn())
+			require.NoError(t, revokeSecretValueFn())
+		})
+	}
 }
