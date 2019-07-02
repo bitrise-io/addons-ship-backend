4d6081a67e59937599d5574d9ed89b3f76a42bd4
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-02 14:12:23 +0200

    Implement service methods

--- dataservices/publish_task_service.go
+++ dataservices/publish_task_service.go
@@ -1,5 +1,9 @@
 package dataservices
 
+import (
+	"github.com/bitrise-io/addons-ship-backend/models"
+)
+
 // PublishTaskService ...
 type PublishTaskService interface {
 	Create(publishTask *models.PublishTask) (*models.PublishTask, error)
--- models/publish_task.go
+++ models/publish_task.go
@@ -5,8 +5,7 @@ import uuid "github.com/satori/go.uuid"
 // PublishTask ...
 type PublishTask struct {
 	Record
-	TaskID    string `json:"task_id"`
-	Completed bool   `json:"completed"`
+	TaskID string `json:"task_id"`
 
 	AppVersionID uuid.UUID  `db:"app_version_id" json:"-"`
 	AppVersion   AppVersion `gorm:"foreignkey:AppVersionID" json:"-"`
