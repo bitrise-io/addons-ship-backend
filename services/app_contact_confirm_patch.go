c56e5696a617cfbb0e046f6925e8928bcc2c097f
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-12 09:29:27 +0200

    Add patch handler skeleton

---
+++ services/app_contact_confirm_patch.go
@@ -0,0 +1,12 @@
+package services
+
+import (
+	"net/http"
+
+	"github.com/bitrise-io/addons-ship-backend/env"
+)
+
+// AppContactConfirmPatchHandler ...
+func AppContactConfirmPatchHandler(env *env.AppEnv, w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
