08662f7b3ea3a57c8a9f4736ef0d9a54779be320
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-12 11:25:05 +0200

    Add confirmed_at clean to compare method

--- models/app_contact_factories_for_test.go
+++ models/app_contact_factories_for_test.go
@@ -18,9 +18,11 @@ func createTestAppContact(t *testing.T, appContact *models.AppContact) *models.A
 func compareAppContacts(t *testing.T, expected, actual models.AppContact) {
 	expected.CreatedAt = time.Time{}
 	expected.UpdatedAt = time.Time{}
+	expected.ConfirmedAt = time.Time{}
 	expected.App = nil
 	actual.CreatedAt = time.Time{}
 	actual.UpdatedAt = time.Time{}
+	actual.ConfirmedAt = time.Time{}
 	actual.App = nil
 	require.Equal(t, expected, actual)
 }
