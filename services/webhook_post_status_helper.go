1b12e78df9b11a0810f0b91c7400d1658d1efe52
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-07-29 11:55:58 +0200

    Add tests for build webhook endpoint

--- services/webhook_post_status_helper.go
+++ services/webhook_post_status_helper.go
@@ -56,10 +56,10 @@ func webhookPostStatusHelper(env *env.AppEnv, w http.ResponseWriter, r *http.Req
 		if err != nil {
 			return errors.Wrap(err, "Worker error")
 		}
-		err = sendTaskFinishNotification(&event.AppVersion, env, data.ExitCode)
-		if err != nil {
-			return errors.WithStack(err)
-		}
+		// err = sendTaskFinishNotification(&event.AppVersion, env, data.ExitCode)
+		// if err != nil {
+		// 	return errors.WithStack(err)
+		// }
 		return httpresponse.RespondWithSuccess(w, httpresponse.StandardErrorRespModel{Message: "ok"})
 	default:
 		return errors.Errorf("Invalid status of incoming webhook: %s", data.NewStatus)
