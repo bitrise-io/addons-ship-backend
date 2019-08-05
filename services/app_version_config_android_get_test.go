fc5a65c16f375acdc9835ecf4abc07fb771b8652
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-08-05 14:51:58 +0200

    Add error handling tests

--- services/app_version_config_android_get_test.go
+++ services/app_version_config_android_get_test.go
@@ -1299,6 +1299,114 @@ func Test_AppVersionAndroidConfigGetHandler(t *testing.T) {
 		})
 	})
 
+	t.Run("when failed to fetch artifact details from API - split APK", func(t *testing.T) {
+		performControllerTest(t, httpMethod, url, handler, ControllerTestCase{
+			contextElements: map[ctxpkg.RequestContextKey]interface{}{
+				services.ContextKeyAuthorizedAppVersionID: testAppVersionID,
+			},
+			env: &env.AppEnv{
+				AppVersionService: &testAppVersionService{
+					findFn: func(appVersion *models.AppVersion) (*models.AppVersion, error) {
+						appVersion.ArtifactInfoData = json.RawMessage(`{"package_name":"myPackage"}`)
+						appVersion.AppStoreInfoData = json.RawMessage(`{"short_description":"Description","full_description":"A bit longer description","whats_new":"This is what is new"}`)
+						appVersion.App = models.App{AppSlug: "test-app-slug", BitriseAPIToken: "test-api-token"}
+						return appVersion, nil
+					},
+				},
+				FeatureGraphicService: &testFeatureGraphicService{
+					findFn: func(featureGraphic *models.FeatureGraphic) (*models.FeatureGraphic, error) {
+						featureGraphic.ID = testFeatureGraphicID
+						featureGraphic.Filename = "feature_graphic.png"
+						featureGraphic.AppVersion = models.AppVersion{
+							Record: models.Record{ID: testAppVersionID},
+							App:    models.App{AppSlug: "test-app-slug"},
+						}
+						return featureGraphic, nil
+					},
+				},
+				AWS: &providers.AWSMock{
+					GeneratePresignedGETURLFn: func(path string, expiration time.Duration) (string, error) {
+						return fmt.Sprintf("http://presigned.url/%s", path), nil
+					},
+				},
+				BitriseAPI: &testBitriseAPI{
+					getAppDetailsFn: func(apiToken, appSlug string) (*bitrise.AppDetails, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return &bitrise.AppDetails{Title: "my-awesome-app"}, nil
+					},
+					getServiceAccountFilesFn: func(apiToken, appSlug string) ([]bitrise.GenericProjectFile, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.GenericProjectFile{bitrise.GenericProjectFile{
+							Slug:        "service-account-slug",
+							DownloadURL: "http://service-account-json.url",
+						}}, nil
+					},
+					getAndroidKeystoreFilesFn: func(apiToken, appSlug string) ([]bitrise.AndroidKeystoreFile, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.AndroidKeystoreFile{
+							bitrise.AndroidKeystoreFile{
+								Slug:        "android-keystore-slug",
+								UserEnvKey:  "ANDROID_KEYSTORE",
+								DownloadURL: "http://android.keystore.url",
+								ExposedMetadataStore: bitrise.ExposedMetadataStore{
+									Password:           "my-secret-password",
+									Alias:              "AnDrOID-KeySTore",
+									PrivateKeyPassword: "my-private-key-pass",
+								},
+							},
+						}, nil
+					},
+					getArtifactsFn: func(apiToken, appSlug, buildSlug string) ([]bitrise.ArtifactListElementResponseModel, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.ArtifactListElementResponseModel{
+							bitrise.ArtifactListElementResponseModel{
+								Title: "app-armeabi-my-awesome-app.apk", Slug: "test-artifact-slug-1",
+								ArtifactMeta: &bitrise.ArtifactMeta{
+									AppInfo: bitrise.AppInfo{AppName: "My Awesome app", VersionName: "1.0", PackageName: "my.package"},
+								},
+							},
+							bitrise.ArtifactListElementResponseModel{
+								Title: "app-x86-my-awesome-app.apk", Slug: "test-artifact-slug-2",
+								ArtifactMeta: &bitrise.ArtifactMeta{
+									AppInfo: bitrise.AppInfo{AppName: "My Awesome app", VersionName: "1.0", PackageName: "my.package"},
+								},
+							},
+						}, nil
+					},
+					getArtifactFn: func(apiToken, appSlug, buildSlug, artifactSlug string) (*bitrise.ArtifactShowResponseItemModel, error) {
+						return nil, errors.New("SOME-BITRISE-API-ERROR")
+					},
+				},
+				AppSettingsService: &testAppSettingsService{
+					findFn: func(appSettings *models.AppSettings) (*models.AppSettings, error) {
+						appSettings.AndroidSettingsData = json.RawMessage(`{"selected_service_account":"service-account-slug","selected_keystore_file":"android-keystore-slug"}`)
+						return appSettings, nil
+					},
+				},
+				ScreenshotService: &testScreenshotService{
+					findAllFn: func(appVersion *models.AppVersion) ([]models.Screenshot, error) {
+						testAppVersion := models.AppVersion{
+							Record: models.Record{ID: testAppVersionID},
+							App:    models.App{AppSlug: "test-app-slug"},
+						}
+						return []models.Screenshot{
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("17ec78c9-e3a8-41ee-b3bd-2df9b4117aa2")}, UploadableObject: models.UploadableObject{Filename: "tv.png"}, ScreenSize: "tv", DeviceType: "TV", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("d5c8564f-eef4-490a-a7fd-8d3050893320")}, UploadableObject: models.UploadableObject{Filename: "wear.png"}, ScreenSize: "wear", DeviceType: "Watch", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("e4d64d18-e414-4fa3-8583-f94a06b4f9a9")}, UploadableObject: models.UploadableObject{Filename: "phone.png"}, ScreenSize: "phone", DeviceType: "Phone", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("4faa287f-afee-46aa-bd6b-553ab11a959c")}, UploadableObject: models.UploadableObject{Filename: "ten_inch.png"}, ScreenSize: "ten_inch", DeviceType: "Tablet", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("27cee0a1-1afd-4280-8d9f-f22526dc3d16")}, UploadableObject: models.UploadableObject{Filename: "seven_inch.png"}, ScreenSize: "seven_inch", DeviceType: "Tablet", AppVersion: testAppVersion},
+						}, nil
+					},
+				},
+			},
+			expectedInternalErr: "SOME-BITRISE-API-ERROR",
+		})
+	})
+
 	t.Run("when artifact doesn't have download URL", func(t *testing.T) {
 		performControllerTest(t, httpMethod, url, handler, ControllerTestCase{
 			contextElements: map[ctxpkg.RequestContextKey]interface{}{
@@ -1353,6 +1461,117 @@ func Test_AppVersionAndroidConfigGetHandler(t *testing.T) {
 				ScreenshotService: &testScreenshotService{
 					findAllFn: func(appVersion *models.AppVersion) ([]models.Screenshot, error) {
 						return []models.Screenshot{}, nil
+					},
+				},
+			},
+			expectedInternalErr: "Failed to get download URL for artifact",
+		})
+	})
+
+	t.Run("when artifact doesn't have download URL - split APK", func(t *testing.T) {
+		performControllerTest(t, httpMethod, url, handler, ControllerTestCase{
+			contextElements: map[ctxpkg.RequestContextKey]interface{}{
+				services.ContextKeyAuthorizedAppVersionID: testAppVersionID,
+			},
+			env: &env.AppEnv{
+				AppVersionService: &testAppVersionService{
+					findFn: func(appVersion *models.AppVersion) (*models.AppVersion, error) {
+						appVersion.ArtifactInfoData = json.RawMessage(`{"package_name":"myPackage"}`)
+						appVersion.AppStoreInfoData = json.RawMessage(`{"short_description":"Description","full_description":"A bit longer description","whats_new":"This is what is new"}`)
+						appVersion.App = models.App{AppSlug: "test-app-slug", BitriseAPIToken: "test-api-token"}
+						return appVersion, nil
+					},
+				},
+				FeatureGraphicService: &testFeatureGraphicService{
+					findFn: func(featureGraphic *models.FeatureGraphic) (*models.FeatureGraphic, error) {
+						featureGraphic.ID = testFeatureGraphicID
+						featureGraphic.Filename = "feature_graphic.png"
+						featureGraphic.AppVersion = models.AppVersion{
+							Record: models.Record{ID: testAppVersionID},
+							App:    models.App{AppSlug: "test-app-slug"},
+						}
+						return featureGraphic, nil
+					},
+				},
+				AWS: &providers.AWSMock{
+					GeneratePresignedGETURLFn: func(path string, expiration time.Duration) (string, error) {
+						return fmt.Sprintf("http://presigned.url/%s", path), nil
+					},
+				},
+				BitriseAPI: &testBitriseAPI{
+					getAppDetailsFn: func(apiToken, appSlug string) (*bitrise.AppDetails, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return &bitrise.AppDetails{Title: "my-awesome-app"}, nil
+					},
+					getServiceAccountFilesFn: func(apiToken, appSlug string) ([]bitrise.GenericProjectFile, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.GenericProjectFile{bitrise.GenericProjectFile{
+							Slug:        "service-account-slug",
+							DownloadURL: "http://service-account-json.url",
+						}}, nil
+					},
+					getAndroidKeystoreFilesFn: func(apiToken, appSlug string) ([]bitrise.AndroidKeystoreFile, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.AndroidKeystoreFile{
+							bitrise.AndroidKeystoreFile{
+								Slug:        "android-keystore-slug",
+								UserEnvKey:  "ANDROID_KEYSTORE",
+								DownloadURL: "http://android.keystore.url",
+								ExposedMetadataStore: bitrise.ExposedMetadataStore{
+									Password:           "my-secret-password",
+									Alias:              "AnDrOID-KeySTore",
+									PrivateKeyPassword: "my-private-key-pass",
+								},
+							},
+						}, nil
+					},
+					getArtifactsFn: func(apiToken, appSlug, buildSlug string) ([]bitrise.ArtifactListElementResponseModel, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						return []bitrise.ArtifactListElementResponseModel{
+							bitrise.ArtifactListElementResponseModel{
+								Title: "app-armeabi-my-awesome-app.apk", Slug: "test-artifact-slug-1",
+								ArtifactMeta: &bitrise.ArtifactMeta{
+									AppInfo: bitrise.AppInfo{AppName: "My Awesome app", VersionName: "1.0", PackageName: "my.package"},
+								},
+							},
+							bitrise.ArtifactListElementResponseModel{
+								Title: "app-x86-my-awesome-app.apk", Slug: "test-artifact-slug-2",
+								ArtifactMeta: &bitrise.ArtifactMeta{
+									AppInfo: bitrise.AppInfo{AppName: "My Awesome app", VersionName: "1.0", PackageName: "my.package"},
+								},
+							},
+						}, nil
+					},
+					getArtifactFn: func(apiToken, appSlug, buildSlug, artifactSlug string) (*bitrise.ArtifactShowResponseItemModel, error) {
+						require.Equal(t, "test-app-slug", appSlug)
+						require.Equal(t, "test-api-token", apiToken)
+						require.Contains(t, []string{"test-artifact-slug-1", "test-artifact-slug-2"}, artifactSlug)
+						return &bitrise.ArtifactShowResponseItemModel{}, nil
+					},
+				},
+				AppSettingsService: &testAppSettingsService{
+					findFn: func(appSettings *models.AppSettings) (*models.AppSettings, error) {
+						appSettings.AndroidSettingsData = json.RawMessage(`{"selected_service_account":"service-account-slug","selected_keystore_file":"android-keystore-slug"}`)
+						return appSettings, nil
+					},
+				},
+				ScreenshotService: &testScreenshotService{
+					findAllFn: func(appVersion *models.AppVersion) ([]models.Screenshot, error) {
+						testAppVersion := models.AppVersion{
+							Record: models.Record{ID: testAppVersionID},
+							App:    models.App{AppSlug: "test-app-slug"},
+						}
+						return []models.Screenshot{
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("17ec78c9-e3a8-41ee-b3bd-2df9b4117aa2")}, UploadableObject: models.UploadableObject{Filename: "tv.png"}, ScreenSize: "tv", DeviceType: "TV", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("d5c8564f-eef4-490a-a7fd-8d3050893320")}, UploadableObject: models.UploadableObject{Filename: "wear.png"}, ScreenSize: "wear", DeviceType: "Watch", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("e4d64d18-e414-4fa3-8583-f94a06b4f9a9")}, UploadableObject: models.UploadableObject{Filename: "phone.png"}, ScreenSize: "phone", DeviceType: "Phone", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("4faa287f-afee-46aa-bd6b-553ab11a959c")}, UploadableObject: models.UploadableObject{Filename: "ten_inch.png"}, ScreenSize: "ten_inch", DeviceType: "Tablet", AppVersion: testAppVersion},
+							models.Screenshot{Record: models.Record{ID: uuid.FromStringOrNil("27cee0a1-1afd-4280-8d9f-f22526dc3d16")}, UploadableObject: models.UploadableObject{Filename: "seven_inch.png"}, ScreenSize: "seven_inch", DeviceType: "Tablet", AppVersion: testAppVersion},
+						}, nil
 					},
 				},
 			},
