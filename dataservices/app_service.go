557f6642baf06a9af8e0e3229d37597702a1de1e
Gergely Bekesi <gergely.bekesi@bitrise.io>
2019-09-03 13:51:41 +0200

    Add update method to app

--- dataservices/app_service.go
+++ dataservices/app_service.go
@@ -6,5 +6,6 @@ import "github.com/bitrise-io/addons-ship-backend/models"
 type AppService interface {
 	Create(*models.App) (*models.App, error)
 	Find(*models.App) (*models.App, error)
+	Update(app *models.App, whitelist []string) (validationErrors []error, dbErr error)
 	Delete(app *models.App) error
 }
