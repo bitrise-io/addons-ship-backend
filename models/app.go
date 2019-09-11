a54ed5234112b719f2d5d36ce9caaad7d7d5f237
Simon Márton <simon.marton@bitrise.io>
2019-09-11 11:56:28 +0200

    Add platform errors to app

--- models/app.go
+++ models/app.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bitrise-io/go-crypto/crypto"
 	"github.com/jinzhu/gorm"
+	"github.com/lib/pq"
 	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 )
@@ -12,14 +13,16 @@ import (
 // App ...
 type App struct {
 	Record
-	AppSlug           string `json:"app_slug"`
-	Plan              string `json:"plan"`
-	BitriseAPIToken   string `json:"-"`
-	APIToken          string `json:"-"`
-	EncryptedSecret   []byte `json:"-"`
-	EncryptedSecretIV []byte `json:"-"`
-	HeaderColor1      string `db:"header_color_1" gorm:"column:header_color_1" json:"header_color_1"`
-	HeaderColor2      string `db:"header_color_2" gorm:"column:header_color_2" json:"header_color_2"`
+	AppSlug           string         `json:"app_slug"`
+	Plan              string         `json:"plan"`
+	BitriseAPIToken   string         `json:"-"`
+	APIToken          string         `json:"-"`
+	EncryptedSecret   []byte         `json:"-"`
+	EncryptedSecretIV []byte         `json:"-"`
+	HeaderColor1      string         `db:"header_color_1" gorm:"column:header_color_1" json:"header_color_1"`
+	HeaderColor2      string         `db:"header_color_2" gorm:"column:header_color_2" json:"header_color_2"`
+	AndroidErrors     pq.StringArray `json:"android_errors" gorm:"type:varchar(128)[]"`
+	IosErrors         pq.StringArray `json:"ios_errors" gorm:"type:varchar(128)[]"`
 
 	AppVersions []AppVersion `gorm:"foreignkey:AppID" json:"app_versions"`
 	AppSettings AppSettings  `gorm:"foreignkey:AppsID" json:"app_settings"`
