ef60a910391ca1510b58f9c423995dea2848bf32
Gergely Bekesi <gergely.bekesi@bitrise.io>
2020-02-12 12:46:31 +0100

    Add enterprise as debug ipa export method

--- bitrise/artifact_model_test.go
+++ bitrise/artifact_model_test.go
@@ -1,6 +1,7 @@
 package bitrise_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bitrise-io/addons-ship-backend/bitrise"
@@ -41,4 +42,18 @@ func Test_ArtifactListElementResponseModel_IsUniversalAPK(t *testing.T) {
 		Title:        "app.universal.apk",
 		ArtifactMeta: &bitrise.ArtifactMeta{Universal: "/somewhere/over/the-rainbow/app.universal.apk"}}.IsUniversalAPK())
 	require.False(t, bitrise.ArtifactListElementResponseModel{Title: "app.apk"}.IsUniversalAPK())
+}
+
+func Test_HasDebugIPAExportMethod(t *testing.T) {
+	for _, exportMethod := range []string{"development", "ad-hoc", "enterprise"} {
+		t.Run(fmt.Sprintf("when distribution type is %s", exportMethod), func(t *testing.T) {
+			require.True(t, bitrise.ArtifactListElementResponseModel{
+				ArtifactMeta: &bitrise.ArtifactMeta{
+					ProvisioningInfo: bitrise.ProvisioningInfo{
+						IPAExportMethod: exportMethod,
+					},
+				},
+			}.HasDebugIPAExportMethod())
+		})
+	}
 }
